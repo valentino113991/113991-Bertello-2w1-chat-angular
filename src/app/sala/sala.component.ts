d0f59f1d5d3a989f2ebd55063d282e81758efa6a
113991-Bertello <113991@tecnicatura.frc.utn.edu.ar>
2024-10-12 08:03:13 -0300

    Added export to Excel and PDF using xlsx, jspdf, and html2canvas

--- src/app/sala/sala.component.ts
+++ src/app/sala/sala.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { MensajeUsuario1Component } from './mensaje-usuario1/mensaje-usuario1.component';
 import { MensajeUsuario2Component } from './mensaje-usuario2/mensaje-usuario2.component';
 import { CommonModule } from '@angular/common';
+import * as XLSX from 'xlsx';
+import { jsPDF } from "jspdf";
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-sala',
@@ -15,5 +18,50 @@ export class SalaComponent {
 
   modificarConversacion(mensaje: string){
     this.mensajes.push(mensaje);
+  }
+
+  fileName= 'ExcelSheet.xlsx';
+
+  exportexcel(){
+    /* pass here the table id */
+    let element = document.getElementById('holaId');
+    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+ 
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+ 
+    /* save to file */  
+    XLSX.writeFile(wb, this.fileName);
+  }
+
+  exportPdf() {
+    // Agarramos la tabla y la asignamos en la variable "elementToPrint"
+    const elementToPrint: any = document.getElementById('holaId');
+    
+    html2canvas(elementToPrint, { scale: 2 }).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4');
+  
+      const pageWidth = pdf.internal.pageSize.getWidth();
+      const pageHeight = pdf.internal.pageSize.getHeight();
+  
+      const imgWidth = pageWidth - 20;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+  
+      const positionY = 10; 
+  
+      pdf.addImage(imgData, 'PNG', 10, positionY, imgWidth, imgHeight);
+  
+      pdf.setProperties({
+        title: 'table',
+        subject: 'Pdf from angular to pdf',
+        author: 'Grupo 6'
+      });
+      
+      pdf.setFontSize(12);
+  
+      pdf.save('myFile.pdf'); //NOMBRE DEL ARCHIVO
+    });
   }
 }
